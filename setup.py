734bfb7138defa4789aa492e62ff4e72c496da2b
Russ <revoltingrobot@gmail.com>
2018-05-25 11:48:17 -0400

    updated setup file to include dependencies

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="csv_to_xlsx",
-    version="0.0.1",
+    version="0.0.5",
     author="Russ Lamb",
     author_email="revoltingrobot@gmail.com",
     description="Convert CSV and TSV to XLSX files using openpyxl",
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/russlamb/file_convert",
     packages=setuptools.find_packages(),
+    install_requires=['openpyxl'],
     classifiers=(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
